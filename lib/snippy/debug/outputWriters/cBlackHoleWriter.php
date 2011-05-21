9b55fcb5a3d48f5187c2d344019d8a924e05779e
Pepa Martinec <joker806@gmail.com>
2011-05-21 15:41:15 +0200

    UPDATE add comment-header to source files

--- lib/snippy/debug/outputWriters/cBlackHoleWriter.php
+++ lib/snippy/debug/outputWriters/cBlackHoleWriter.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of snippy.
+ *
+ * @author Josef Martinec <joker806@gmail.com>
+ * @copyright Copyright (c) 2011, Josef Martinec
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace snippy\debug\outputWriters;
 
 use snippy\debug\iOutputItem;
